6181e55783f783cb7a27247323524567a1fecf48
mslail <meetinderlail@yahoo.ca>
2020-01-18 14:51:41 -0500

    Added SGD

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import random
 import sys
 import numpy as np
 from numpy import dot
@@ -7,10 +9,13 @@ def create_output(path, data):
     new_file = open(path, "w+")
     for line in data:
         new_file.write(str(line) + "\n")
+    new_file.close()
     
 def main(data_file, json_file):
-    data = open(data_file)
-    json = open(json_file)
+    data = open(data_file, 'r')
+    json_data =  open(json_file, 'r')
+    config = json.load(json_data)
+
     X = []
     y = []
     for line in data:
@@ -23,16 +28,31 @@ def main(data_file, json_file):
     X = np.array(X,dtype=float)
     y = np.array(y,dtype=float).transpose()
     Xt = X.transpose()
-    w = dot(dot(inv(dot(Xt, X)), Xt), y)
-    create_output(data_file.replace('in', 'out'), w)
+    
+    w_anayltic = dot(dot(inv(dot(Xt, X)), Xt), y)
 
-    print(w)
+    M, N = X.shape
+    w_sgd = np.ones(N, dtype=float)
+    alpha = config['learning rate']
+    num_iter = config['num iter']
+    i = 0
+    n = 0
+    while n < num_iter:
+        i = random.randrange(M-1)
+        xi = X[i]
+        yi = y[i]
+        w_sgd += alpha*(yi-dot(w_sgd.transpose(), xi))*xi
+        n += 1
+
+    output = np.concatenate((w_anayltic, [""], w_sgd), axis=0)
+    create_output(data_file.replace('in', 'out'), output)
+
 
 if __name__ == "__main__":
     try:
         files = sys.argv
         data_file = files[1]
-        json_file = files[1]
+        json_file = files[2]
         main(data_file, json_file)
     except Exception as e:
         raise Exception('Error: {}'.format(e))
